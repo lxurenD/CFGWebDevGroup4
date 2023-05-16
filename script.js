7494feb08ba2e62062d2b11e92eaa1dd4f6e9af7
lxurenD <92858618+lxurenD@users.noreply.github.com>
2023-05-16 16:52:01 +0100

    finished project

---
+++ script.js
@@ -0,0 +1,9 @@
+function cookieBye(){
+    document.getElementById("cookie").style.display = "none";
+    document.getElementById("cookieText").style.display = "block";
+}
+
+function cookieHi(){
+    document.getElementById("cookie").style.display = "block";
+    document.getElementById("cookieText").style.display = "none";
+}
